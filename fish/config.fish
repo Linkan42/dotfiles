af336b91cd5b4e9900800c777bb6abfc54decd91
Linus S. <sali20jt@student.ju.se>
2024-10-28 21:39:33 +0100

    Initial commit

---
+++ fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+export XDG__RUNTIME_DIR=/run/user/1000
+mkdir -p $XDG_RUNTIME_DIR
+chmod 700 $XDG_RUNTIME_DIR
+
+fish_add_path /home/linus/.dotnet/tools
